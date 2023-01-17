6b24015b9243699f1df42014c03de72d4667046d
stefanus.suwandi <stefanus.suwandi@binus.edu>
2023-01-17 21:49:51 +0700

    added features section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     #home,
     #movies,
     #features {
-        height: 90vh;
+        height: 100vh;
     }
 
     #home {
@@ -45,17 +45,54 @@
 
     #features .content {}
 
-    #features,
-    #movies {
+    #features {
         background-image: none;
         background: linear-gradient(179.78deg, #17120F 25.79%, #44312B 83.31%);
         backdrop-filter: blur(2px);
     }
 
+    #movies {
+        background-image: none;
+        background: linear-gradient(179.78deg, #44312B 25.79%, #17120F 83.31%);
+        backdrop-filter: blur(2px);
+    }
+
     #footer-container {
-        background: #44312B;
+        background: #17120F;
         backdrop-filter: blur(2px);
         width: 100%;
+    }
+
+    @media only screen and (max-width: 990px) {
+        #feature-cards {
+            display: flex;
+            flex-direction: column;
+            align-items: stretch;
+        }
+
+        #features {
+            height: max-content;
+        }
+
+        .card {
+            margin-top: 24px;
+        }
+    }
+
+    .card {
+        border-radius: 2em !important;
+        text-align: center;
+        box-shadow: 0 5px 10px rgba(0, 0, 0, .2);
+    }
+
+    .card img {
+        border-radius: 2em !important;
+    }
+
+    .card:hover {
+        box-shadow: 1px 8px 20px rgba(255, 255, 255, 0.466);
+        -webkit-transition: box-shadow .2s ease-in-out;
+        cursor: pointer;
     }
 </style>
 
@@ -65,7 +102,6 @@
     <div id="home" class="container">
         <div class="content">
             <h2 class="text-white fw-bold">Browse movies, see what people say, and give your opinions !</h2>
-            <p class="text-white">Discover new movies</p>
             <div class="mt-3">
                 <a href="/register" class="btn btn-warning">Sign up</a>
                 <a href="/login" class="btn btn-outline-warning">Sign in</a>
@@ -76,6 +112,39 @@
     <div id="features">
         <div class="content container pt-5">
             <h2 class="text-light text-center fw-bold">FEATURES</h2>
+
+            <div id="feature-cards" class="d-flex justify-content-evenly align-items-center mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ url('/images/discover-movies.png') }}" style="height: 250px;" class="card-img-top"
+                        alt="image">
+                    <div class="card-body">
+                        <h4 class="card-text fw-bold">Discover movies</h4>
+                        <p>Discover latest and popular movie details, including movie cover, synopsis, genre, and trailer.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ url('/images/see-comments.png') }}" style="height: 250px;" class="card-img-top"
+                        alt="image">
+                    <div class="card-body">
+                        <h4 class="card-text fw-bold">See review</h4>
+                        <p>Get to know what people say and rate about every movie before you watch it.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ url('/images/give-comments.png') }}" style="height: 250px;" class="card-img-top"
+                        alt="image">
+                    <div class="card-body">
+                        <h4 class="card-text fw-bold">Give review</h4>
+                        <p>Rate and give your most honest opinion for the movie you have watched.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     </div>

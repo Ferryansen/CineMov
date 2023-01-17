9c49eb91e7da60b63a4a336f6a9330413582c629
stefanus.suwandi <stefanus.suwandi@binus.edu>
2023-01-17 21:00:32 +0700

    add initial landing page

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,15 +1,57 @@
-<nav class="navbar navbar-expand-lg navbar-dark">
+<style>
+    #landing-nav {
+        display: flex;
+        flex-direction: row;
+    }
+
+    #landing-nav a:hover {
+        text-decoration: underline;
+    }
+
+    @media only screen and (max-width: 767px) {
+        #landing-nav {
+            display: none;
+        }
+
+        #landing-nav-mobile {
+            display: block !important;
+        }
+    }
+</style>
+
+<nav class="navbar navbar-expand-md navbar-dark">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="/"><span class="text-warning">Cine</span><span>Mov.</span></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav ms-auto">
-            @auth
-            @yield('searchbar')
-            <li class="nav-item dropdown">
+        {{-- Left --}}
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul class="navbar-nav me-auto">
+                @auth
+                    <a class="navbar-brand fw-bold" href="/home"><span class="text-warning">Cine</span><span>Mov.</span></a>
+                @else
+                    <a class="navbar-brand fw-bold" href="/"><span class="text-warning">Cine</span><span>Mov.</span></a>
+                @endauth
+            </ul>
+        </div>
+
+        {{-- Center --}}
+        <div>
+            @if (request()->is('/'))
+                <div id="landing-nav">
+                    <a class="nav-link text-light mx-3" href="#home">Home</a>
+                    <a class="nav-link text-light mx-3" href="#features">Features</a>
+                    <a class="nav-link text-light mx-3" href="#movies">Movies</a>
+                </div>
+            @endif
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".dual-collapse2">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+        {{-- Right --}}
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ms-auto">
+                @auth
+                    @yield('searchbar')
+                    <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
                             {{ auth()->user()->name }}
@@ -28,6 +70,24 @@
                         </ul>
                     </li>
                 @else
+                    <div id="landing-nav-mobile" style="display:none">
+                        <li class="nav-item">
+                            <a href="#home" class="nav-link">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#features" class="nav-link">
+                                Features
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#movies" class="nav-link">
+                                Movies
+                            </a>
+                        </li>
+                    </div>
                     <li class="nav-item">
                         <a href="/login" class="nav-link {{ request()->is('login') ? 'active' : '' }}">
                             Sign In
@@ -39,8 +99,8 @@
                         </a>
                     </li>
                 @endauth
-
             </ul>
         </div>
     </div>
+
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,95 @@
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        width: 100%
+    }
+
+    #home,
+    #movies,
+    #features {
+        height: 90vh;
+    }
+
+    #home {
+        display: flex;
+        flex-direction: row;
+        justify-content: start;
+        align-items: center;
+    }
+
+    @media only screen and (max-width: 990px) {
+        #home {
+            justify-content: center;
+            align-items: center;
+        }
+
+        #home .content {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            text-align: center;
+        }
+
+        body {
+            background-size: cover !important;
+        }
+    }
+
+    #home .content {
+        width: 50%;
+    }
+
+    #features .content {}
+
+    #features,
+    #movies {
+        background-image: none;
+        background: linear-gradient(179.78deg, #17120F 25.79%, #44312B 83.31%);
+        backdrop-filter: blur(2px);
+    }
+
+    #footer-container {
+        background: #44312B;
+        backdrop-filter: blur(2px);
+        width: 100%;
+    }
+</style>
+
 @extends('layout')
 
 @section('content')
-    <h1 class="text-white">Landing page</h1>
+    <div id="home" class="container">
+        <div class="content">
+            <h2 class="text-white fw-bold">Browse movies, see what people say, and give your opinions !</h2>
+            <p class="text-white">Discover new movies</p>
+            <div class="mt-3">
+                <a href="/register" class="btn btn-warning">Sign up</a>
+                <a href="/login" class="btn btn-outline-warning">Sign in</a>
+            </div>
+        </div>
+    </div>
+
+    <div id="features">
+        <div class="content container pt-5">
+            <h2 class="text-light text-center fw-bold">FEATURES</h2>
+
+        </div>
+    </div>
+
+    <div id="movies">
+        <div class="content container pt-5">
+            <h2 class="text-light text-center fw-bold">MOVIES</h2>
+
+        </div>
+    </div>
+@endsection
+
+@section('footer')
+    <div id="footer-container" class="d-flex justify-content-center align-items-center">
+        @include('footer')
+    </div>
 @endsection
